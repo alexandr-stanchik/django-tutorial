2a89377b46329e1f6d66f42287a3b75d1954b7d7
alexandr-stanchik <lex023ott@mail.ru>
2023-02-26 02:08:35 +0300

    Tutorial02

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'polls',
+    # 'polls',
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -106,7 +107,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Minsk'
+# TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
